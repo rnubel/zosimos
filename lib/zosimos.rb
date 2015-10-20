960b4476ef93ca9ebbbb0a04937c25c6bd380dcc
Robert Nubel <rnubel@enovafinancial.com>
2015-10-19 22:51:04 -0400

    Prototype functionality.

--- lib/zosimos.rb
+++ lib/zosimos.rb
@@ -1,5 +1,36 @@
-require "zosimos/version"
+require 'zosimos/version'
+require 'csv'
+require 'alchemy_api'
 
 module Zosimos
-  # Your code goes here...
+  extend self
+
+  def process(input, method: :sentiment_analysis)
+    output = []
+    headers = input.first
+
+    output << headers.dup + [ 'sentiment', 'score' ]
+
+    input.drop(1).each do |row|
+      output_row = row.dup
+      result = alchemy_call(method, text: row.last, language: default_language)
+
+      output_row << result['type']
+      output_row << result['score']
+
+      output << output_row
+    end
+
+    output
+  end
+
+  private
+
+  def alchemy_call(*args)
+    AlchemyAPI.search(*args)
+  end
+
+  def default_language
+    'english'
+  end
 end
--- spec/zosimos_spec.rb
+++ spec/zosimos_spec.rb
@@ -5,7 +5,18 @@ describe Zosimos do
     expect(Zosimos::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'runs a dataset through sentiment analysis' do
+    input = File.read('spec/data/input.csv')
+
+    CSV.parse(input, headers: true) do |row|
+      expect(AlchemyAPI).to receive(:search)
+        .with(:sentiment_analysis, text: row['text'], language: 'english')
+        .and_return({"score" => "-0.5", "type" => "negative"})
+    end
+
+    output = Zosimos.process(CSV.parse(input), method: :sentiment_analysis)
+
+    expect(output.count).to eq(4)
+    expect(output[1].join(',')).to eq('1,1,I really hate this. All of it sucks.,negative,-0.5')
   end
 end
